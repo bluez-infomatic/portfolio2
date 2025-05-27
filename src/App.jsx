87b92c3e9732f6cdb97084ee8904fa269b279884
Ganesh1412-developer <ganeshbabu.2101055@srec.ac.in>
2025-05-27 10:41:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import Notfound from "./pages/Notfound";
+// import Notfound from "./pages/Notfound";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route index element={<Home />} />
-        <Route path="*" element={<Notfound />} />
+{/*         <Route path="*" element={<Notfound />} /> */}
       </Routes>
     </BrowserRouter>
   );
