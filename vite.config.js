8bda7c3b3c8b80a9ab2b057e21a0f82965ab4742
bluez-infomatic <sanjaibluez@gmail.com>
2025-05-28 15:51:34 +0530

    s

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' 
-import path from "path";
-// https://vite.dev/config/
+
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve:{
-    alias:{
-      "@":path.resolve(__dirname, "./src"),
-    },
-  },
-  base:"/portfolio2/",
-});
+  base: './', // or '/' if deploying to root
+  plugins: [react()],
+})
